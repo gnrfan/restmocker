db57a70c4f7ccad15d026b43880f0c326fdae95f
Antonio Ognio <gnrfan@gnrfan.org>
2011-09-14 12:04:19 -0500

    Working a bit on the admin for documents

--- apps/documents/admin.py
+++ apps/documents/admin.py
@@ -5,6 +5,22 @@ from django.contrib import admin
 from documents import strings, constants
 from documents.models import Realm, Document, TextSubstitution
 
-admin.site.register(Realm)
-admin.site.register(Document)
+from documents import strings
+
+class RealmAdmin(admin.ModelAdmin):
+    list_display = ('name', 'prefix', 'document_count', 'created', )
+    search_fields = ('name', 'description', )
+
+    def document_count(self, instance):
+        return instance.document_set.count()
+    document_count.short_description = strings.DOCUMENT_COUNT_SD
+
+admin.site.register(Realm, RealmAdmin)
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'realm', 'use_attachment', 'created', )
+    search_fields = ('title', 'realm', 'description', )
+    list_filter = ('realm', 'use_attachment', )
+
+admin.site.register(Document, DocumentAdmin)
 admin.site.register(TextSubstitution)
