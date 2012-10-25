e49b18de5f65d5d219fd831a09c3f7cee93115eb
Antonio Ognio <gnrfan@gnrfan.org>
2012-10-25 13:21:53 -0500

    Updating codebase to Django 1.4

--- project/settings/__init__.py
+++ project/settings/__init__.py
@@ -88,13 +88,13 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'urls'
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.auth',
+    'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.request',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
+    'django.core.context_processors.static',
     'django.core.context_processors.debug',
     'django.contrib.messages.context_processors.messages',
-    'common.context_processors.paths',
     'common.context_processors.current_site',
 )
 
